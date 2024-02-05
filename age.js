d72ccefc018ed62e32f6ffa626451fd2dfb8eede
unknown <benwaiganjo66@gmail.com>
2024-02-05 16:02:45 +0300

    all

---
+++ age.js
@@ -0,0 +1,12 @@
+
+function canDrive(age) {
+  let age = 18;
+  if ((age => 30) {
+    console.log("can drive");
+  } elseif ( age ===17)
+  { console.log('very close')}
+  else{ console.log('wi mwana gui uyu');}
+}
+canDrive(35) 
+console.log(canDrive (38));
+console.log('nindknmas')
---
+++ driver.js
@@ -0,0 +1,2 @@
+console.log("lan");
+const driver = amana;
